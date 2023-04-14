d90b00b2ce5b4212029a85f717b60d290e7c19ee
IVIaike <trainer.ekb@gmail.com>
2023-04-14 13:06:36 +0500

    2nd commit

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,12 +1,26 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class Application {
 
     public static void main(String[] args) throws SQLException {
-       try (Connection connection = getConnection()) {
+ //       String query = "SELECT * FROM employee WHERE id=2";
+        String query = "SELECT city.city_name, employee.first_name, employee.last_name FROM city JOIN employee ON city.city_id = employee.city_id WHERE id=2";
 
+       try (Connection connection = getConnection();
+            PreparedStatement statement = connection.prepareStatement(query);
+            ResultSet resultSet = statement.executeQuery()) {
+
+           while (resultSet.next()){
+               String firstName = resultSet.getString(2);
+               String lastName = resultSet.getString(3);
+               String gendre = resultSet.getString(4);
+               String age = resultSet.getString(5);
+               System.out.println((firstName + " " + lastName + " " + gendre + " " + age));
+           }
+       }catch (SQLException e) {
+           System.out.println("Ошибка при подключении к базе данных!");
+           e.printStackTrace();
+           // Исключение для обработки возможных ошибок при подключении
        }
     }
 
